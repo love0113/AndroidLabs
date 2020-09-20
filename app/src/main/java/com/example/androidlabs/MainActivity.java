cf551478d7eb3f70816bfea92cf841852dafcdca
tmac0113 <huan0113@algonquinlive.com>
2020-09-19 23:18:00 -0400

    update

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_linear);
 
         Button btn = findViewById(R.id.button4);
         btn.setOnClickListener(v ->
